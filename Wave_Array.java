510d85d5bd582001a59c82812edcab69bdcec4f7
Archit Anant <architannat5@gmail.com>
2021-06-27 15:29:32 +0530

    first commit

---
+++ Wave_Array.java
@@ -0,0 +1,15 @@
+package google.interview;
+
+public class Wave_Array {
+    public static void convertToWave(int[] arr, int n){
+        for (int i=1;i<n/2+1;i++){
+            int k = i*2-1;
+            int f = arr[k];
+            arr[k] = arr[k-1];
+            arr[k-1]=f;
+        }
+        for (int k: arr){
+            System.out.print(k+" ");
+        }
+    }
+}
